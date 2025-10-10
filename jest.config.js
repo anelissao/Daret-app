ab355b7f4dd0cdbfb7ad165c1741b767e6862666
anelissao <anouarelissaoui20@gmail.com>
2025-10-10 10:30:00 +0100

    Initial project setup with dependencies and configuration

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/__tests__/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.js'
+  ],
+  verbose: true
+};
